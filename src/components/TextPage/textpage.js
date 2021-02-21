37a7f2b9203dd36c1c135c3029777d78a7bbcb2e
dora-su <dorasu02@gmail.com>
2021-02-21 06:13:28 -0500

    mergE

--- src/components/TextPage/textpage.js
+++ src/components/TextPage/textpage.js
@@ -207,6 +207,7 @@ class TextPage extends React.Component {
                         <div class="text-container">
                             {text}
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             <button onClick={this.toChanged}className="inclusify-btn btn-1">Inclusify</button>
             <button className="inclusify-btn " onClick={() => {navigator.clipboard.writeText(this.state.input_text)}}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
@@ -214,6 +215,9 @@ class TextPage extends React.Component {
   <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
 </svg>
             </button>
+=======
+                            <button onClick={this.toChanged}className="inclusify-btn">Inclusify</button>
+>>>>>>> Stashed changes
 =======
                             <button onClick={this.toChanged}className="inclusify-btn">Inclusify</button>
 >>>>>>> Stashed changes
