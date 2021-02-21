00801376cdb7ce8572b8ef8acf58b018c0047aad
Udy Ding <udy.ding@gmail.com>
2021-02-20 23:35:39 -0500

    added vocab

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+require("dotenv").config();
 // const {loadAllWords} = require("./loadWords")
-const {readAll} = require("./readWords")
+const { readAll } = require("./readWords");
 
 // reads in an array of words
 async function checkWords(words) {
-    readAll(words)
+  readAll(words);
 }
-checkWords()
+checkWords();
